b019146c10969e8abb3d7a0ba28a43b776d1d1b1
Doanh Ly <ly.do@husky.neu.edu>
2019-08-28 18:13:36 -0700

    added id to input

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -9,18 +9,18 @@ export default () => {
       <Header>
         Contact Me
       </Header>
-      <form name='contact' action='/success' method="post" data-netlify="true"
+      <form action='/success' method="post" data-netlify="true"
             data-netlify-honeypot="bot-field" className={styles.form}>
-        <input type="hidden" name="form-name" value="Contact Form" />
+        <input type="hidden" name="form-name" value="contact-form" />
         <input type="hidden" name="bot-field" />
         <p className={styles.formIdentity}>
-          <input type="text" name="name" placeholder='Name' style={{fontSize: '24px'}}/>      
-          <input type="email" name="email" placeholder='Email' style={{fontSize: '24px'}}/>       
+          <input id="name" type="text" name="name" placeholder='Name' style={{fontSize: '24px'}}/>      
+          <input id="email" type="email" name="email" placeholder='Email' style={{fontSize: '24px'}}/>       
         </p>
         <p className={styles.message}>
           <textarea style={{fontSize: '24px'}}
             rows="15" cols="15" name="message" 
-            placeholder='Leave me a message!' />           
+            placeholder='Leave me a message!'/>           
         </p>
         <button className={styles.button} type="submit">Send &#8594;</button>
       </form> 
